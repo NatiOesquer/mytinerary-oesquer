6550b53f69367d10186997a4667e3e3b3f085aae
NatiOesquer <94532066+NatiOesquer@users.noreply.github.com>
2023-09-09 18:36:28 -0300

    activities (#13)

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,9 +1,37 @@
 
+import { useSelector, useDispatch } from "react-redux"
+import activity_actions from "../store/actions/activities"
+const { read_activities_from_itinerary } = activity_actions
+import { useEffect, useState } from "react"
+import NotFoundItinerary from "../components/NotFoundItinerary"
 
-export default function Activity() {
-  return (
-    <div>
-        <div className="md:w-[600px] h-[350px] w-[280px] h-[150px]items-center bg-cover p-3 m-5 flex justify-center bg-no-repeat" style={{ backgroundImage: `url(../img/under.png)` }}></div>
-    </div>
+
+
+
+export default function Activity({ itinerary_id }) {
+
+  const dispatch = useDispatch()
+  let activities = useSelector(store => store.activities.activities_from_itinerary)
+  console.log(activities)
+  // let name = useSelector((store =>store.activities?.activities_from_itinerary));
+  // console.log(name)
+
+  useEffect(
+    () => {
+      dispatch(read_activities_from_itinerary({ itinerary_id: itinerary_id }))
+
+    }, []
   )
+  return (
+    <>
+        <div className="w-[350px] h-[300px] flex items-center  justify-start bg-slate-100 mb-4 sm:w-[750px] ">
+            {activities?.map(each => <div key={each._id} className="w-5/12 flex flex-col rounded-xl h-[190px] items-center p-2 m-4 bg-slate-400">
+                <img className="w-[240px] h-[130px]" src={each.photo} alt={each.name} />
+                <p className="text-[16px] font-bold">{each.name}</p>
+            </div>
+            )}
+        </div>
+    </>
+)
+
 }
--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -3,18 +3,18 @@ import Activity from "./Activity"
 import Bills from "./Bills"
 
 
-export default function Itinerary({ name, price, duration, tags, photo, admin_id, admin_photo }) {
+export default function Itinerary({ name, price, duration, tags, photo, admin_id, admin_photo, id }) {
 
     const [show, setShow] = useState(false)
-
+    
     const bill = Math.floor(price);
     const time = Math.floor(duration / 60)
     const [heart, setHeart] = useState(false)
-
+    
     return (
         <main>
             <div className="flex flex-col items-center justify-center mt-5 px-20">
-                <div className="w-[350px] h-[850px] flex flex-grow flex-col items-center justify-start bg-slate-100 mb-4 md:w-[750px]">
+                <div className="w-[350px] h-[850px] flex flex-grow flex-col items-center justify-start bg-slate-100 md:w-[750px]">
                     <p className=" flex items-center justify-center mb-5 text-[28px] font-bold m-4">{name}</p>
                     <div className=" w-[350px] h-[500px] flex items-center justify-center bg-slate-700 md:w-[650px]">
                         <img className="w-[350px] h-[500px] md:w-[650px]" src={photo} alt="photo" />
@@ -53,8 +53,8 @@ export default function Itinerary({ name, price, duration, tags, photo, admin_id
                         {show ? (<div onClick={() => setShow(!show)} className=" sm:flex"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" /></svg></div>) : (<div onClick={() => setShow(!show)} className=" sm:flex"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" /></svg></div>)}
                     </div>
                 </div>
-                {show ? <Activity /> : <div></div>}
-
+                 {show && <Activity itinerary_id={id}/>} 
+                
             </div>
         </main>
 
